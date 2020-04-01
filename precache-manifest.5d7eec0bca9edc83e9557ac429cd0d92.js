f5fa9ef5191d94095b3270769b744b695903b281
bulbulahmed9 <bulbulahmed.tk@gmail.com>
2020-04-01 10:21:21 +0600

    Updates

---
+++ precache-manifest.5d7eec0bca9edc83e9557ac429cd0d92.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb4ca966fa65fe4387351dd7cf1beb3e",
+    "url": "/testapp/index.html"
+  },
+  {
+    "revision": "3c9ee17976efbbde475d",
+    "url": "/testapp/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "e9eb4aa229ad4845ed15",
+    "url": "/testapp/static/js/2.28ced02f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/testapp/static/js/2.28ced02f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c9ee17976efbbde475d",
+    "url": "/testapp/static/js/main.4091524c.chunk.js"
+  },
+  {
+    "revision": "e78fe68a9952c36db1bc",
+    "url": "/testapp/static/js/runtime-main.d9cea74a.js"
+  }
+]);
