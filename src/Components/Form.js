4e6d3496dde7240cc0a6625fa6b1cbb6fd8f06d5
bulbulahmed9 <bulbulahmed.tk@gmail.com>
2020-04-01 10:01:18 +0600

    initial commit

---
+++ src/Components/Form.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Form = () => {
+    return (
+        <div>
+           Hello User, You can't log in  :)
+        </div>
+    )
+}
+
+export default Form
---
+++ src/Components/Modal.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Form from './Form'
+
+
+export default function FormDialog() {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div>
+      <Button variant="outlined" color="inherit" onClick={handleClickOpen}>
+        Log in
+      </Button>
+      <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
+        <DialogContent>
+          {/* <DialogContentText>
+            To subscribe to this website, please enter your email address here. We will send updates
+            occasionally.
+          </DialogContentText>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="name"
+            label="Email Address"
+            type="email"
+            fullWidth
+          /> */}
+          <Form />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            Cancel
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+}
